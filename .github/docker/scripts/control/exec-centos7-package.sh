c2d283d7928c3df6616cd3b59ce87cd76e11fafa
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-05 14:54:27 -0400

    Twentieth try

--- .github/docker/scripts/control/exec-centos7-package.sh
+++ .github/docker/scripts/control/exec-centos7-package.sh
@@ -6,5 +6,5 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -dit $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c "$HIRS_DOCKER_SCRIPTS_BUILDER_DIR/package-centos.sh" && echo "1" || echo "0"
+docker exec -it $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c "$HIRS_RELATIVE_ROOT_DIR/package-centos.sh" && echo "1"
 
