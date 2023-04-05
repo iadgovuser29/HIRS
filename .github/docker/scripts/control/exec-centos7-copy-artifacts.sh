102314c77497d265e25fcd954fd4b7c4e719d561
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-05 14:14:04 -0400

    Fifteenth try

--- .github/docker/scripts/control/exec-centos7-copy-artifacts.sh
+++ .github/docker/scripts/control/exec-centos7-copy-artifacts.sh
@@ -6,5 +6,6 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -dit $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c "find $HIRS_RELATIVE_ROOT_DIR/package/rpm/RPMS/ -iname \"*el7*rpm\" $BUILDER_OUTPUT_DIR" && echo "1" || echo "0"
+docker exec -dit $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c "find $HIRS_RELATIVE_ROOT_DIR/package/rpm/RPMS/ -iname \"*el7*rpm\" -exec cp {} $BUILDER_OUTPUT_DIR \;" && echo "1" 
+
 
