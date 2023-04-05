ffa12acd22e3971b5a23a7af61dfa25ed7e0f8a2
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-05 13:11:07 -0400

    Fourteenth try

--- .github/docker/scripts/builder/package-centos.sh
+++ .github/docker/scripts/builder/package-centos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -x # echo commands as they are executed
 
-APP_HOME="`dirname "$0"`"
+APP_HOME=$(realpath "`dirname "$0"`")
 SETUP_SCRIPT="$APP_HOME""/setup-builder.sh"
 
 source $SETUP_SCRIPT
--- .github/docker/scripts/control/exec-centos7-copy-logs.sh
+++ .github/docker/scripts/control/exec-centos7-copy-logs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -x # echo commands as they are executed
 
-APP_HOME="`dirname "$0"`"
+APP_HOME=$(realpath "`dirname "$0"`")
 DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
-docker exec -it $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c "cp $HIRS_RELATIVE_ROOT_DIR/ -iname \"*log\" $BUILDER_OUTPUT_DIR" && echo "1" || echo "0"
+source $DOCKER_SCRIPTS_COMMON
+
+docker exec -dit $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c "cp $HIRS_RELATIVE_ROOT_DIR/ -iname \"*log\" $BUILDER_OUTPUT_DIR" && echo "1" || echo "0"
 
