3d50c81ee6f7c951be2e2c4175761625b90e80b6
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-05 12:48:31 -0400

    Eleventh try

--- .github/docker/scripts/common.sh
+++ .github/docker/scripts/common.sh
@@ -9,7 +9,7 @@
 ##       common.sh
 ##       control/
 #######
-DOCKER_SCRIPTS_HOME=$(realpath "`dirname "$0"`")
+DOCKER_SCRIPTS_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
 
 BUILDER_REPO_DIR="/repo"
 BUILDER_WORKING_REPO_DIR="/HIRS"
@@ -24,7 +24,7 @@ GITHUB_IMAGE_CENTOS7_BUILDER="ghcr.io/nsacyber/hirs/centos7-builder"
 HOST_OUTPUT_DIR="/tmp/hirs-builders/"
 HOST_CENTOS7_OUTPUT_DIR="$HOST_OUTPUT_DIR""centos7/"
 
-HIRS_RELATIVE_ROOT_DIR="$DOCKER_SCRIPTS_HOME""/../../"
+HIRS_RELATIVE_ROOT_DIR="$DOCKER_SCRIPTS_HOME""/../../../"
 HIRS_DOCKER_SCRIPTS_BUILDER_DIR="$DOCKER_SCRIPTS_HOME""/builder/"
 HIRS_DOCKER_SCRIPTS_CONTROL_DIR="$DOCKER_SCRIPTS_HOME""/control/"
 
