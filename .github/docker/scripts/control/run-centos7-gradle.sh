d956b42f8ebe9c7789037214380c94fc0c0aadec
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-06 12:40:50 -0400

    46

--- .github/docker/scripts/control/run-centos7-gradle.sh
+++ .github/docker/scripts/control/run-centos7-gradle.sh
@@ -7,5 +7,11 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 source $DOCKER_SCRIPTS_COMMON
 
 # See the common script for assume file structure of the repo.
-docker run -dit -v "$HOST_CENTOS7_OUTPUT_DIR":"$BUILDER_OUTPUT_DIR" -v "$HIRS_RELATIVE_ROOT_DIR":"$BUILDER_REPO_DIR":ro --name="$DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME" $GITHUB_IMAGE_CENTOS7_BUILDER /bin/bash -c "$BUILDER_REPO_DIR/.github/docker/scripts/builder/setup-builder.sh && tail -f /dev/null" && echo "1" || echo "0"
+docker run -dit -v "$HOST_CENTOS7_OUTPUT_DIR":"$BUILDER_OUTPUT_DIR" -v "$HIRS_RELATIVE_ROOT_DIR":"$BUILDER_REPO_DIR":ro --name="$DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME" $GITHUB_IMAGE_CENTOS7_BUILDER /bin/bash -c "$BUILDER_REPO_DIR/.github/docker/scripts/builder/setup-builder.sh && tail -f /dev/null"
+
+# Wait for builder to be ready
+until [ "`docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash ls $BUILDER_WORKING_REPO_DIR`" ]; do
+    sleep 5;
+done;
+echo "$DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME ready."
 
