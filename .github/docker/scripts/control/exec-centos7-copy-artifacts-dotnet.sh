11b3725b1ab63a9155157e59bdf70c49bddefbc4
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-06 16:42:51 -0400

    50

--- .github/docker/scripts/control/exec-centos7-copy-artifacts-dotnet.sh
+++ .github/docker/scripts/control/exec-centos7-copy-artifacts-dotnet.sh
@@ -6,6 +6,6 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME "/bin/bash" "-c" "find $BUILDER_WORKING_REPO_DIR/HIRS_Provisioner.NET/ -iname *.rpm -o -iname *.deb -o -iname *.msi -o -iname *.zip -exec cp {} $BUILDER_OUTPUT_DIR \;"  
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME "/bin/bash" "-c" "'find $BUILDER_WORKING_REPO_DIR/HIRS_Provisioner.NET/ -iname *.rpm -o -iname *.deb -o -iname *.msi -o -iname *.zip -exec cp {} $BUILDER_OUTPUT_DIR \;'"  
 
 ls -al $BUILDER_OUTPUT_DIR
