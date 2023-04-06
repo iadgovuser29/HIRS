30fd148c7237fac1f774e2468469a7ed5c57d8c5
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-06 16:42:51 -0400

    50

--- .github/docker/scripts/control/exec-centos7-copy-artifacts-gradle.sh
+++ .github/docker/scripts/control/exec-centos7-copy-artifacts-gradle.sh
@@ -6,5 +6,5 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME "/bin/bash" "-c" "find $BUILDER_WORKING_REPO_DIR/package/ -iname *.rpm -o -iname *.deb -exec cp {} $BUILDER_OUTPUT_DIR \;"
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME "/bin/bash" "-c" "'find $BUILDER_WORKING_REPO_DIR/package/ -iname *.rpm -o -iname *.deb -exec cp {} $BUILDER_OUTPUT_DIR \;'"
 
