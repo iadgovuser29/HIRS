47442960d3203da8a781ff90ea99ac77ea2bef6b
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-06 12:40:50 -0400

    46

--- .github/docker/scripts/control/exec-centos7-gradlew-build.sh
+++ .github/docker/scripts/control/exec-centos7-gradlew-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +x # echo commands as they are executed
+set -x # echo commands as they are executed
 
 APP_HOME=$(realpath "`dirname "$0"`")
 DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
@@ -8,5 +8,5 @@ source $DOCKER_SCRIPTS_COMMON
 
 cmd="'$BUILDER_WORKING_REPO_DIR/.github/docker/scripts/builder/gradlew-build.sh'"
 
-docker exec $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c $cmd
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME /bin/bash -c $cmd
 
