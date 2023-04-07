56f46497465ff6066783c09899d6dddf22c1ba78
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-07 13:15:40 -0400

    57

--- .github/docker/scripts/control/exec-centos7-dotnet-package-deb.sh
+++ .github/docker/scripts/control/exec-centos7-dotnet-package-deb.sh
@@ -6,5 +6,5 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME "/bin/bash" "-c" "'$BUILDER_WORKING_REPO_DIR/.github/docker/scripts/builder/dotnet-deb.sh'"
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME "/bin/bash" "-c" "'$BUILDER_WORKING_REPO_DIR/.github/docker/scripts/builder/dotnet-deb.sh'"
 
