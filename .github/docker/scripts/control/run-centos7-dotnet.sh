28dcb356ef0e6de9c6bfc0e664df2c726ca19ec6
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-07 13:10:58 -0400

    56

--- .github/docker/scripts/control/run-centos7-dotnet.sh
+++ .github/docker/scripts/control/run-centos7-dotnet.sh
@@ -9,7 +9,7 @@ source $DOCKER_SCRIPTS_COMMON
 docker run -dit -v "$HOST_CENTOS7_OUTPUT_DIR":"$BUILDER_OUTPUT_DIR" -v "$HIRS_RELATIVE_ROOT_DIR":"$BUILDER_REPO_DIR":ro --name="$DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME" $GITHUB_IMAGE_CENTOS7_BUILDER /bin/bash -c "$BUILDER_REPO_DIR/.github/docker/scripts/builder/setup-builder.sh && tail -f /dev/null"
 
 # Ensure scripts are executable
-docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME find $BUILDER_WORKING_REPO_DIR/.gradle/docker/scripts -type f -iname "*.sh" -exec chmod +x {} \;
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME find $BUILDER_WORKING_REPO_DIR/.github/docker/scripts -type f -iname "*.sh" -exec chmod +x {} \;
 
 # Wait for builder to be ready
 until [ "`docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME ls -alR $BUILDER_WORKING_REPO_DIR/.github/docker/scripts`" ]; do
