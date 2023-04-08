f1315e9b60d1bc343479f45c3e2cf8fdab3048df
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-08 09:32:37 -0400

    62

--- .github/docker/scripts/control/exec-centos7-copy-logs-dotnet.sh
+++ .github/docker/scripts/control/exec-centos7-copy-logs-dotnet.sh
@@ -6,5 +6,5 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME "find" "'$BUILDER_WORKING_REPO_DIR/HIRS_Provisioner.NET'" "-iname" "\"*log\"" "-exec" "cp" "{}" "'$BUILDER_OUTPUT_DIR'" "\;"
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_DOTNET_NAME 'find' "'$BUILDER_WORKING_REPO_DIR/HIRS_Provisioner.NET'" "-iname" '*log' '-exec' 'cp' '{}' "'$BUILDER_OUTPUT_DIR'" '\;'
 
