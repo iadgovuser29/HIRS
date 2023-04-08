0711ff557c4343d8bed3468302cd82d86a0e9446
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-04-08 10:02:01 -0400

    64

--- .github/docker/scripts/control/exec-centos7-copy-logs-gradle.sh
+++ .github/docker/scripts/control/exec-centos7-copy-logs-gradle.sh
@@ -6,5 +6,5 @@ DOCKER_SCRIPTS_COMMON="$APP_HOME""/../common.sh"
 
 source $DOCKER_SCRIPTS_COMMON
 
-docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME find $BUILDER_WORKING_REPO_DIR/ -type f -iname *log -exec cp {} $BUILDER_OUTPUT_DIR \;
+docker exec -i $DOCKER_CONTAINER_CENTOS7_BUILDER_GRADLE_NAME find $BUILDER_WORKING_REPO_DIR/ -type f -iname '*log' -exec cp {} $BUILDER_OUTPUT_DIR \;
 
